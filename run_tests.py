7994c6f0e3a53a8c2280332c06d46724ffbea36f
Alex <alexdodd@gmail.com>
2020-11-26 11:21:38 +0000

    tests and setup init

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+
+import unittest
+from tests.fizzbuzz_test import TestFizzbuzz
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/fizzbuzz_test.py
@@ -0,0 +1,6 @@
+import unittest
+from src.fizzbuzz import Fizzbuzz
+
+class TestFizzbuzz(unittest.TestCase):
+    def setUp(self):
+        pass
